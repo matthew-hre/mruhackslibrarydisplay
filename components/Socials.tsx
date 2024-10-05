22449efedd8c9700084f6084dbc44bc9c57998b1
Matthew Hrehirchuk <matthew_hre@outlook.com>
2024-10-05 07:50:56 -0600

    changed insta to qr

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,21 +1,20 @@
-import { FaInstagram } from "react-icons/fa";
-
 import DiscordQR from "@/app/discord.png";
 import HackerpackQR from "@/app/hackerpack.png";
+import InstaQR from "@/app/insta.png";
 
 export function Socials() {
   return (
     <>
       <div className="rounded-lg bg-white p-8">
-        <h2 className="text-xl font-bold text-black pb-2">Connect With Us</h2>
-        <div className="flex flex-col gap-3">
+        <h2 className="text-2xl font-bold text-black pb-2">Connect With Us</h2>
+        <h2 className="text-xl font-bold text-black pb-2">
+          Follow our Instagram
+        </h2>
+        <div className="flex flex-col gap-3 pb-4">
           <div className="flex flex-row gap-2 items-center">
-            <FaInstagram size={24} />
-            <span className="text-semibold text-xl">@mruhacks</span>
+            <img src={InstaQR.src} alt="Instagram Logo" className="w-24 h-24" />
           </div>
         </div>
-      </div>
-      <div className="rounded-lg bg-white p-8">
         <h2 className="text-xl font-bold text-black pb-2">Join the Discord</h2>
         <div className="flex flex-col gap-3 pb-4">
           <div className="flex flex-row gap-2 items-center">
