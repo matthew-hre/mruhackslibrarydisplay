b769d95e4eecb7a4722e4ac5b2bd272403113206
Matthew Hrehirchuk <matthew_hre@outlook.com>
2024-10-05 07:06:36 -0600

    fixed issue with utc/mst time zones

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,22 @@ export default async function Home() {
     <div className="font-[family-name:var(--font-geist-sans)] w-full h-screen bg-gradient-to-br from-blue-500 to-purple-500 gap-4 p-4 flex flex-row">
       <div className="flex flex-col flex-1 gap-4">
         {(() => {
-          const hours = new Date().getHours();
+          const now = new Date();
+          const mstTimePart = new Intl.DateTimeFormat("en-US", {
+            timeZone: "America/Denver",
+            hour: "2-digit",
+            hour12: false,
+          })
+            .formatToParts(now)
+            .find((part) => part.type === "hour");
+          if (!mstTimePart) {
+            return (
+              <h1 className="text-2xl font-bold text-white">
+                Time not available
+              </h1>
+            );
+          }
+          const hours = parseInt(mstTimePart.value, 10);
           let greeting;
           if (hours >= 6 && hours < 9) {
             greeting = "Rise and Shine Hackers!";
